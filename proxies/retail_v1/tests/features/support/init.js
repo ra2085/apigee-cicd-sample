461860e2aff1d929a809db962d90e999c176d13f
Ruben <rgonzalez@corpfolder.com>
2018-05-17 22:20:37 -0500

    a ver.

--- proxies/retail_v1/tests/features/support/init.js
+++ proxies/retail_v1/tests/features/support/init.js
@@ -5,7 +5,6 @@ const {defineSupportCode} = require('cucumber');
 
 defineSupportCode(function({Before}) {
     Before(function() {
-        console.log(this.parameters.proxyEndpoint);
         this.apickli = new apickli.Apickli('https', this.parameters.proxyEndpoint);
         
         this.apickli.addRequestHeader('Cache-Control', 'no-cache');
